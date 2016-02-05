a532753c5c3df1a481765a5f3def47e275ea7597
Varun Ganapathi <varung@gmail.com>
2016-02-05 04:11:40 -0500

    installer scripts

---
+++ install_go.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+gotgz="https://storage.googleapis.com/golang/go1.5.2.linux-amd64.tar.gz"
+wget $gotgz
+tar -C /usr/local -xzf $(basename $gotgz)
+mkdir -p $HOME/go
+echo "export GOPATH=$HOME/go" >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> ~/.bashrc
