f632b3d3d7bb6e9f0d9fbd42d27def8b8dc8b1bb
Varun Ganapathi <varun@cloudlabs.io>
2016-02-10 06:37:48 -0500

    forced to version 1.9.7 for lua

--- build_nginx.sh
+++ build_nginx.sh
@@ -22,10 +22,29 @@ sudo make install
 apt-get install -y libssl-dev
 
 cd /root
-wget http://nginx.org/download/nginx-1.9.11.tar.gz
-tar -xzvf nginx-1.9.11.tar.gz 
-cd nginx-1.9.11
+wget http://luajit.org/download/LuaJIT-2.0.3.tar.gz
+tar -xzvf LuaJIT-2.0.3.tar.gz 
+cd LuaJIT-2.0.3
+make;
+make install;
 
-./configure --sbin-path=/usr/local/nginx/nginx --conf-path=/usr/local/nginx/nginx.conf --pid-path=/usr/local/nginx/nginx.pid --with-pcre=../pcre-8.37 --with-zlib=../zlib-1.2.8  --with-http_ssl_module  --with-stream_ssl_module --with-stream --with-mail
+export LUAJIT_LIB=/usr/local/lib/
+export LUAJIT_INC=/usr/local/include/luajit-2.0/
 
-make 
+cd /root;
+wget https://github.com/simpl/ngx_devel_kit/archive/v0.2.19.tar.gz
+tar -xzvf v0.2.19.tar.gz 
+
+cd /root;
+wget https://github.com/openresty/lua-nginx-module/archive/v0.10.0.tar.gz
+tar -xzvf v0.10.0.tar.gz 
+
+cd /root
+wget wget http://nginx.org/download/nginx-1.9.7.tar.gz
+tar -xzvf nginx-1.9.7.tar.gz 
+cd nginx-1.9.7
+
+./configure --sbin-path=/usr/local/nginx/nginx --conf-path=/usr/local/nginx/nginx.conf --pid-path=/usr/local/nginx/nginx.pid --with-ld-opt="-Wl,-rpath,/usr/local/lib" --with-pcre=../pcre-8.37 --with-zlib=../zlib-1.2.8  --with-http_ssl_module  --with-stream_ssl_module --with-stream --add-module=/root/ngx_devel_kit-0.2.19 --add-module=/root/lua-nginx-module-0.10.0
+
+make  -j 2
+make install;
