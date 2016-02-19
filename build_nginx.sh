570bede81496c293cde88254706ba98d8fa47816
Varun Ganapathi <varun@cloudlabs.io>
2016-02-19 04:09:29 -0500

    full tar ball build

--- build_nginx.sh
+++ build_nginx.sh
@@ -45,8 +45,24 @@ tar -xzvf nginx-1.9.7.tar.gz
 
 
 
+cd /root
+wget http://www.kyne.com.au/~mark/software/download/lua-cjson-2.1.0.tar.gz
+tar -xzvf lua-cjson-2.1.0.tar.gz
+cd lua-cjson-2.1.0 
+CPATH=/usr/local/include/luajit-2.0/ make
+
 cd /root/nginx-1.9.7
 ./configure --sbin-path=/usr/local/nginx/nginx --conf-path=/usr/local/nginx/nginx.conf --pid-path=/usr/local/nginx/nginx.pid --with-ld-opt="-Wl,-rpath,SSORIGINS/lib" --with-pcre=../pcre-8.37 --with-zlib=../zlib-1.2.8  --with-http_ssl_module  --with-stream_ssl_module --with-stream --add-module=/root/ngx_devel_kit-0.2.19 --add-module=/root/lua-nginx-module-0.10.0
 
 make  -j 2
 make install;
+
+cp /root/lua-cjson-2.1.0/cjson.so /usr/local/nginx/lib/
+cp /usr/local/lib/libluajit-5.1.so.2 /usr/local/nginx/lib/
+cd /usr/local/nginx/
+for i in `LD_VERBOSE=1 LD_TRACE_LOADED_OBJECTS=1 ./nginx -v | grep "=>"| awk '{ print $4}' | sort -u | grep so`; do cp $i lib/; done
+
+chrpath /usr/local/nginx/nginx -r '${ORIGIN}/lib'
+tar -czvf nginx.tgz /usr/local/nginx
+
+
