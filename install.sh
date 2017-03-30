072cd5367b4ef8696f5782be6f767d686c4909e2
Varun Ganapathi <varun@udacity.com>
2017-03-30 14:32:38 -0700

    create $HOME/backup if not present

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 if [ -z "$HOME" ]; then
   HOME=/root
 fi
+mkdir $HOME/.backup
 cp -r .vimrc $HOME/
 mkdir $HOME/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
